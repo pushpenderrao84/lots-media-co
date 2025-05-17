0d34bde40a292cd0bd8802f903942762c8129e86
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-17 14:29:15 +0530

    Update XLogo.jsx

--- src/components/icons/XLogo.jsx
+++ src/components/icons/XLogo.jsx
@@ -1,14 +1,14 @@
 const XLogo = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 72 72"
+    viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <path d="M42.5,31.2L66,6h-6L39.8,27.6L24,6H4l24.6,33.6L4,66h6l21.3-22.8L48,66h20L42.5,31.2z M12.9,10h8l38.1,52h-8L12.9,10z" />
+    <path d="M14.2,10.4L22,2h-2.2l-7.4,7.8L6,2H2l8.6,11.6L2,22h2.2l7.9-8.4L18,22h4l-7.8-11.6z M4.6,4h3.2l13.5,18h-3.2L4.6,4z" />
   </svg>
 );
 
