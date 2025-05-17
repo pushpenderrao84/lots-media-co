cd678bc7479a8fd96bfb7dba9d424173a2548c21
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-17 14:16:28 +0530

    Update XLogo.jsx

--- src/components/icons/XLogo.jsx
+++ src/components/icons/XLogo.jsx
@@ -1,9 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
 const XLogo = (props) => (
 <svg 
     version="1.1"   
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="3840"
+    height="2160"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
