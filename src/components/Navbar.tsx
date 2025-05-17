d1d61749f0891603718a577ac0e1f4a37896484f
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-17 12:49:12 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,8 @@ const Navbar = () => {
           <img 
             src="/lovable-uploads/a6e0b97c-98e4-46fd-bb8d-0ef459a48468.png" 
             alt="LOTS Media Logo" 
-            className="h-12 mr-2 border-2 border-black rounded-md"
+            className="h-12 mr-2 outline outline-2 rounded-md"
+            style={{ outlineColor: '#333333' }}
           />
         </a>
 
