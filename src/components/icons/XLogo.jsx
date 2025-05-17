ac460ac7c9cfb4e6824f743cd0a469570d5a2ff6
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-17 14:31:23 +0530

    Update XLogo.jsx

--- src/components/icons/XLogo.jsx
+++ src/components/icons/XLogo.jsx
@@ -9,7 +9,7 @@ const XLogo = (props) => (
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <path d="M4 4l16 16M20 4L4 20" />
+    <path d="M14.2,10.4L22,2h-2.2l-7.4,7.8L6,2H2l8.6,11.6L2,22h2.2l7.9-8.4L18,22h4l-7.8-11.6z M4.6,4h3.2l13.5,18h-3.2L4.6,4z" />
   </svg>
 );
 
