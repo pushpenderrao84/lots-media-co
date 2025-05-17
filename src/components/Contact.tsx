843fb79956a7d5c83db5a78e13a0f1cc191426c0
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-17 14:16:56 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -107,7 +107,7 @@ const Contact = () => {
                   </div>
                   <div>
                     <p className="font-medium">X</p>
-                    <p className="text-charcoal/70">@lotsmediaco</p>
+                    <p className="text-charcoal/70">@lotsmediacooooo</p>
                   </div>
                 </a>
               </div>
