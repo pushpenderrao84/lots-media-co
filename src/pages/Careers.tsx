73b91bb5548c1ae6afb52cfb805bbc871bb5ce82
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-21 19:53:41 +0530

    Update Careers.tsx

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,1 +1,6 @@
+import React from 'react';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
+export default Careers;
