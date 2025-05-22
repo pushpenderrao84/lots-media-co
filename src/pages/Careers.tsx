ea4621cd3e2b75eea790fb19e4b003bf56f08aa6
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-22 19:36:41 +0530

    Update Careers.tsx

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Button } from "@/components/ui/button";
 
 const Careers = () => {
   return (
