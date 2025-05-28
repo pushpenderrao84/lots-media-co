26c3887ec9fac70ac73dc1452a8e0f13bd0e4b3e
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-28 14:45:08 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import { cn } from "@/lib/utils";
 
 const About = () => {
