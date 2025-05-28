8d3a091f6098aa0f72b86a8b7b4319e4338bda75
Pushpender Rao <stfuidgafiykyk@gmail.com>
2025-05-28 14:21:01 +0530

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/Navbar";
+import Footer from "@/Footer";
 import { Card, CardContent } from "@/components/ui/card";
 
 const testimonials = [
